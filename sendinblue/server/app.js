3d2a33ace37c57f2f35eaf5172c126ec63e2d592
waldteufelUkrNet <waldteufel@ukr.net>
2022-02-05 00:41:30 +0200

    готово

--- sendinblue/server/app.js
+++ sendinblue/server/app.js
@@ -37,9 +37,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/sendinblue', sendinbluesRouter);
+app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- sendinblue/server/routes/sendinblue.js
+++ sendinblue/server/routes/sendinblue.js
@@ -1,101 +1,54 @@
 const express        = require('express'),
-      router         = express.Router();
+      router         = express.Router(),
+      SibApiV3Sdk    = require('sib-api-v3-sdk');
 
-var SibApiV3Sdk = require('sib-api-v3-sdk');
 SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = process.env.SENDINBLUE_API_KEY;
 
-
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('sendinbluetest', { title: 'Express' });
 });
 
 router.get('/send', function(req, res, next) {
-  res.status(200).send('start sending ...');
 ////////////////////////////////////////////////////////////////////////////////
-console.log('here start to testing');
+  let emailSettings = {
+    senderEmail : 'waldteufel@ukr.net',
+    senderName  : 'Жабка',
+    userEmail   : 'vadim405120@gmail.com',
+    userName    : 'vadim',
+  };
 
+  let emailOptions = {
+    billType       : 'canceled', // not_paid / paid / partially_paid / canceled
+    billEndDate    : '04.02.2022 - 13:44',
+    billNumber     : 'Wjhj873hjQ',
+    userName       : 'Вернигородський-Синьовертий-Смазнов Едуард',
+    userPhone      : '+380672207067',
+    billImgSrc     : 'http://wtwt.pp.ua/temp/atlas.png',
+    billHeader     : 'Atlas Weekend 2022',
+    billDescription: 'Абонемент на 3 дні (8-10 липня) Кількість квитків 2, VIP-Standing на 1 день (9 липня) Кількість квитків 2, Абонемент VIP-Standing на 2 дні (7–8 липня) Кількість квитків 1',
+    billPrice      : '₴ 17 102',
+    bill1Sum       : '₴ 0,00',
+    bill2Sum       : '₴ 17 102',
+    staticFilesSrc : 'http://wtwt.pp.ua/temp/',
+    subject        : 'Ваша квитанція'
+  };
 
-new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
-  {
-    'subject':'Hello from the Node SDK!',
-    'sender' : {'email':'waldteufel@ukr.net', 'name':'Sendinblue'},
-    'replyTo' : {'email':'waldteufel@ukr.net', 'name':'waldteufel'},
-    'to' : [{'name': 'vadim', 'email':'vadim405120@gmail.com'}],
-    'htmlContent' : '<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>',
-    'params' : {'bodyMessage':'Made just for you!'}
-  }
-).then(function(data) {
-  console.log(data);
-}, function(error) {
-  console.error(error);
-});
+  res.status(200).render('email.pug', emailOptions, function(err, html){
+    if (err) throw err;
 
+    new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail(
+      {
+        'subject'     : emailOptions.subject,
+        'sender'      : {'email':emailSettings.senderEmail, 'name': emailSettings.senderName},
+        'to'          : [{'email': emailSettings.userEmail, 'name': emailSettings.userName}],
+        'htmlContent' : html
+      }
+    )
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+
+  });
 ////////////////////////////////////////////////////////////////////////////////
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express        = require('express'),
-//       router         = express.Router(),
-
-//       SibApiV3Sdk    = require('sib-api-v3-sdk'),
-//       defaultClient  = SibApiV3Sdk.ApiClient.instance,
-//       apiKey         = defaultClient.authentications['api-key'],
-//       apiInstance    = new SibApiV3Sdk.EmailCampaignsApi(),
-//       emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
-
-// apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('sendinbluetest', { title: 'Express' });
-// });
-
-// router.get('/send', function(req, res, next) {
-//   res.status(200).send('start sending ...');
-// ////////////////////////////////////////////////////////////////////////////////
-// console.log('here start to testing');
-
-// emailCampaigns.name        = 'Campaign sent via the API';
-// emailCampaigns.subject     = 'My subject';
-// emailCampaigns.sender      = {'name': 'From name', 'email':'waldteufel@ukr.net'};
-// emailCampaigns.type        = 'classic';
-// emailCampaigns.htmlContent = 'Congratulations! Bla-bla-bla)';
-// emailCampaigns.recipients  = {listIds: [2]};
-// // emailCampaigns.scheduledAt = '2018-01-01 00:00:01';
-
-// apiInstance.createEmailCampaign(emailCampaigns)
-//            .then(function(data) {
-//              console.log('API called successfully. Returned data: ' + JSON.stringify(data) );
-//            }, function(error) {
-//              console.error(error);
-//            });
-
-// ////////////////////////////////////////////////////////////////////////////////
-// });
-
-// module.exports = router;
